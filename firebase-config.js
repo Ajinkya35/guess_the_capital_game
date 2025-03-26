ee83688af8bff294edb685e1ad8b5c675246d9eb
Ajinkya Matre <84321604+Ajinkya35@users.noreply.github.com>
2025-03-27 03:14:20 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,9 +6,10 @@ var firebaseConfig = {
     projectId: "cc-lab5-3a401",
     storageBucket: "cc-lab5-3a401.firebasestorage.app",
     messagingSenderId: "1010585184726",
+    databaseURL: "https://cc-lab5-3a401-default-rtdb.firebaseio.com",
     appId: "1:1010585184726:web:fea20ff418c1ae21c05c77",
     measurementId: "G-E9RVMEC8KR"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  
+  const database = firebase.database();
